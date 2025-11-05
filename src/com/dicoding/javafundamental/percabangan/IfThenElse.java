d043a166133e0f4f36dd6c18da19f3687be6d172
EdoFA12 <edo.broo12@gmail.com>
2025-11-05 21:56:25 +0700

    Initial commit

---
+++ src/com/dicoding/javafundamental/percabangan/IfThenElse.java
@@ -0,0 +1,14 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThenElse {
+    public static void main(String[] args){
+        boolean isOn = false;
+
+        if (isOn){
+            System.out.println("Nyalakan lampu");
+        } else{
+            System.out.println("Lampu mati/ Kondisi tidak terpenuhi.....");
+        }
+
+    }
+}
